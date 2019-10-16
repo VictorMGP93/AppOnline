1442e41249962c5d31fdff7db1af8a20cb13a905
Victor Manuel <victorrevenge@hotmail.com>
2019-10-16 15:04:56 -0500

    cambio1

---
+++ AppOnline/Models/Citas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppOnline.Models
+{
+    public class Citas
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Nombre{ get; set; }
+
+
+    }
+}
